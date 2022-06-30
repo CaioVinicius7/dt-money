c60acc536c8ef3878113dc2524d788c9466d05ad
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-30 17:52:01 -0300

    Salvando dados do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 
 import closeImg from "../../assets/close.svg";
@@ -13,7 +13,16 @@ interface ModalProps {
 }
 
 function NewTransactionModal({ isOpen, onRequestClose }: ModalProps) {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(title, value, category, type);
+  }
 
   return (
     <Modal
@@ -31,11 +40,21 @@ function NewTransactionModal({ isOpen, onRequestClose }: ModalProps) {
         />
       </button>
 
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2> Cadastrar transação </h2>
 
-        <input type="text" placeholder="Título" />
-        <input type="number" placeholder="valor" />
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="valor"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -63,7 +82,12 @@ function NewTransactionModal({ isOpen, onRequestClose }: ModalProps) {
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input type="text" placeholder="Categoria" />
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <button type="submit">Cadastrar</button>
       </Container>
