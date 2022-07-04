4254d3e17cd8af86443b29e3f3a6e539ed952a24
Caio Vinícius <caio1525pereira@gmail.com>
2022-07-04 11:38:48 -0300

    Carregando transações

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
-import { TransactionsContext } from "./TransactionsContext";
+import { TransactionsProvider } from "./TransactionsContext";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
@@ -23,7 +23,7 @@ function App() {
   }
 
   return (
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -33,7 +33,7 @@ function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
