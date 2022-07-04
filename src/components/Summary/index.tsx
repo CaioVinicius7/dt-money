8e9782d75119e14746c47af3c417d51357ae1103
Caio Vinícius <caio1525pereira@gmail.com>
2022-07-04 12:27:02 -0300

    Calculando resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -10,6 +10,25 @@ import { TransactionsContext } from "../../TransactionsContext";
 function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -17,7 +36,12 @@ function Summary() {
           <p> Entradas </p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong> R$1000.00 </strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.deposits)}
+        </strong>
       </div>
 
       <div>
@@ -25,15 +49,26 @@ function Summary() {
           <p> Saídas </p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong> - R$5000.00 </strong>
+        <strong>
+          -
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.withdraws)}
+        </strong>
       </div>
 
       <div className="highlight-background">
         <header>
-          <p> Entradas </p>
+          <p> Total </p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong> R$5000.00 </strong>
+        <strong>
+          {new Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(summary.total)}
+        </strong>
       </div>
     </Container>
   );
