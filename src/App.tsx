b6a6a806d526d1f891ea0ef9619885e34393fb11
Caio Vinícius <caio1525pereira@gmail.com>
2022-07-04 12:32:26 -0300

    Criando hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
